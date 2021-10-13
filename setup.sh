76c198ec3c65cefe9d16a170d212e04bea9256b3
Darren Miller <darren@dmlogic.net>
2021-10-13 17:31:25 +0100

    composer

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@ sudo apt udpate
 sudo apt install python3-pygame
 sudo apt install php-cli php-curl php-intl php-mbstring php-gb php-imap php-sqlite3 php-gd
 sudo systemctl disable apache2
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
